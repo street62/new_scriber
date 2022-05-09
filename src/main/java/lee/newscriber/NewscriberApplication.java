0bdc43f844cb3402b9f0b8110285e986f4853a64
Senglee Choi <tmdfl1027@gmail.com>
2022-05-09 11:27:39 +0900

    initialize project

---
+++ src/main/java/lee/newscriber/NewscriberApplication.java
@@ -0,0 +1,13 @@
+package lee.newscriber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewscriberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lee/newscriber/NewscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.newscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
