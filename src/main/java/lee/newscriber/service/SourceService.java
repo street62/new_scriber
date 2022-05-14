ff6f96e8134c3a80233e09a949fd75f744e30bf2
Senglee Choi <tmdfl1027@gmail.com>
2022-05-14 20:44:12 +0900

    [#4] Feat: SourceService 클래스 작성
    - loadSourceList(): sourceRepository.getSourceList() 호출해 소스 리스트 가져옴.
    - loadArticlesFrom():
    1) updateSource() 메서드를 통해 해당 소스의 글들을 업데이트
    2) sourceRepository.getArticlesFrom() 메서드를 통해 업데이트 된 글들을 repository에서 받아옴.
    - updateSource(): Jsoup 라이브러리를 사용해 해당 사이트를 크롤링하고, 추가된 데이터를 repository로 전달해 업데이트

---
+++ src/main/java/lee/newscriber/service/SourceService.java
@@ -0,0 +1,54 @@
+package lee.newscriber.service;
+
+import lee.newscriber.domain.Article;
+import lee.newscriber.domain.Source;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SourceService {
+    SourceRepository sourceRepository;
+    public ResponseStatus addNewSource(NewSourceRequest newSourceRequest) {
+//        String url = newSourceRequest.getUrl();
+//        SourceType type = newSourceRequest.getSourceType();
+//
+//        ArrayList<SourceSampleData> sourceSampleData = newSourceRequest.getSampleData();
+
+    }
+
+    public ResponseEntity<List<Source>> loadSourceList() {
+        sourceRepository.getSourceList();
+    }
+
+    public ResponseEntity<List<Article>> loadArticlesFrom(long sourceId) {
+        updateSource(sourceId);
+        sourceRepository.getArticlesFrom(sourceId);
+    }
+
+    private void updateSource(long sourceId) throws IOException {
+        Source source = sourceRepository.getSource(sourceId);
+        List<String> urlList = new ArrayList<>();
+        Connection conn = Jsoup.connect("https://keepseeking.tistory.com/");
+        Document document = conn.get();
+        Elements titleElements = document.getElementsByClass("title-post");
+        for (Element element : titleElements) {
+            System.out.println(element);
+        }
+    }
+
+
+}
