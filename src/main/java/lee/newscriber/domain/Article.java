0c2d06ef0635328a6042789cacdab6ae703b43d3
Senglee Choi <tmdfl1027@gmail.com>
2022-05-19 22:18:26 +0900

    [#5] Feat: Article, Source 엔티티 클래스 작성
    Article(Many) - Source(One)으로 엔티티 연관관계 매핑.

--- src/main/java/lee/newscriber/domain/Article.java
+++ src/main/java/lee/newscriber/domain/Article.java
@@ -1,16 +1,24 @@
 package lee.newscriber.domain;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
 public class Article {
+    @Id @GeneratedValue
     private long articleId;
-    private long sourceId;
+    @ManyToOne
+    @JoinColumn(name = "SOURCE_ID")
+    private Source source;
     private String url;
     private String title;
     private String content;
 
-    public Article(long sourceId, String title, String content) {
-        this.sourceId = sourceId;
+    public Article() {
+
+    }
+    public Article(Source source, String title, String content) {
+        this.source = source;
         this.title = title;
         this.content = content;
     }
--- src/main/java/lee/newscriber/domain/Source.java
+++ src/main/java/lee/newscriber/domain/Source.java
@@ -1,9 +1,21 @@
 package lee.newscriber.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Source {
+    @Id
+    @Column(name = "SOURCE_ID")
     private long id;
     private String title;
     private String url;
     private String titleTag;
     private String contentTag;
+    @OneToMany(mappedBy = "article")
+    private List<Article> articles = new ArrayList<>();
 }
